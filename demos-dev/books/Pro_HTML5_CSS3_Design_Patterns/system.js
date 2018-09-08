f68e8f6007dbaba99cdf28de427f3f4983091103
An Pham <anpham6@gmail.com>
2018-09-08 11:00:24 -0700

    SystemJS setting to not transpile TypeScript into ES2015.

---
+++ demos-dev/books/Pro_HTML5_CSS3_Design_Patterns/system.js
@@ -0,0 +1,26 @@
+var androme = null;
+
+SystemJS.config({
+    packages: {
+        '/build': { defaultExtension: 'js' }
+    },
+    map: {
+        'plugin-babel': '/node_modules/systemjs-plugin-babel/plugin-babel.js',
+        'systemjs-babel-build': '/node_modules/systemjs-plugin-babel/systemjs-babel-browser.js'
+    },
+    meta: {
+       '*.js': {
+           babelOptions: {
+               es2015: false
+           }
+       }
+   },
+   transpiler: 'plugin-babel'
+});
+System.import('/build/main.js').then(function(result) {
+    androme = result;
+    androme.parseDocument().then(function() {
+        androme.close();
+        androme.saveAllToDisk();
+    });
+});
